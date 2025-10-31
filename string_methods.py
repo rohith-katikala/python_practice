16997abb4a67fb088769eb3d3e45ce37915a4cf9
rohith-katikala <sairohitkatikila@gmail.com>
2025-10-31 18:12:13 +0530

    string methods

---
+++ string_methods.py
@@ -0,0 +1,17 @@
+# string methods
+
+name = input("Enter your full name: ")
+
+# result = len(name)
+# result = name.find("i")
+# result = name.rfind("i")
+# result = name.capitalize()
+# result = name.upper()
+# result = name.lower()
+# result = name.isalpha()
+# result = name.isdigit()
+
+
+
+
+print(result)
