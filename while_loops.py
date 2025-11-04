ffaec9589256ffdd9a8865f5aa78c61444078d40
rohith-katikala <sairohitkatikila@gmail.com>
2025-11-04 19:02:51 +0530

    Initial Push

---
+++ while_loops.py
@@ -0,0 +1,8 @@
+# while loop = execute some code WHILE some condition remains true
+
+name = input("Enter your name: ")
+
+while name == "":
+    print("You did not enter your name")
+    name = input("Enter your name:")
+print(f"Hello {name}")
