f5ae5ded7dd8276cf00739cc43aa37257658aefb
rohith-katikala <sairohitkatikila@gmail.com>
2025-11-04 14:24:43 +0530

    Initial Push

--- string_indexing.py
+++ string_indexing.py
@@ -4,7 +4,7 @@
 credit_number = "1234-5678-9012-3456"
 # last_digits = credit_number[-4:]
 # print(f"XXXX-XXXX-XXXX-{last_digits}")
-
+#
 # print(credit_number[0])
 # print(credit_number[:4])
 # print(credit_number[5:9])
